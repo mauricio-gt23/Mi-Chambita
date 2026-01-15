2b70402c2e150eca2282957d63d7d8cec028a463
Mauricio <mauricio_carmen01@hotmail.com>
2026-01-15 01:13:43 -0500

    feat(data): add local-first methods to MovimientoRepository

--- app/src/main/java/com/michambita/data/repository/MovimientoRepository.kt
+++ app/src/main/java/com/michambita/data/repository/MovimientoRepository.kt
@@ -3,5 +3,7 @@ package com.michambita.data.repository
 import com.michambita.domain.model.Movimiento
 
 interface MovimientoRepository {
-    suspend fun sincronizarMovimientos(movimientos: List<Movimiento>): Result<Unit>
+    suspend fun saveMovimientoLocally(movimiento: Movimiento): Result<Unit>
+    
+    suspend fun syncPendingMovimientosToFirebase(): Result<Unit>
 }
