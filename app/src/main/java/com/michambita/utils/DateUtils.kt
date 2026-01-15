464d1ff3706ecb6fee60b4ed0c9406fcf1fd44de
Mauricio <mauricio_carmen01@hotmail.com>
2026-01-15 01:40:31 -0500

    feat(utils): add isSameDay method to DateUtils

--- app/src/main/java/com/michambita/utils/DateUtils.kt
+++ app/src/main/java/com/michambita/utils/DateUtils.kt
@@ -16,6 +16,16 @@ object DateUtils {
         return c.get(Calendar.YEAR) == y && c.get(Calendar.MONTH) == m && c.get(Calendar.DAY_OF_MONTH) == d
     }
 
+    fun isSameDay(date1: Date, date2: Date): Boolean {
+        val cal1 = Calendar.getInstance()
+        cal1.time = date1
+        val cal2 = Calendar.getInstance()
+        cal2.time = date2
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
+                cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH)
+    }
+
     fun formatDate(date: Date): String {
         val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
         return sdf.format(date)
