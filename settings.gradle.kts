782a5f2893bbbfc756061a691f7d830e309c2d7a
Mauricio Carmen <mauricio_carmen01@hotmail.com>
2026-03-02 23:35:04 -0500

    feat: Modules were added to settings-gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google()
         gradlePluginPortal()
@@ -16,3 +17,12 @@ dependencyResolutionManagement {
 
 rootProject.name = "MiChambita"
 include(":app")
+include(":core:domain")
+include(":core:common")
+include(":core:data")
+include(":core:ui")
+include(":router")
+include(":feature:auth")
+include(":feature:home")
+include(":feature:producto")
+include(":feature:inventario")
